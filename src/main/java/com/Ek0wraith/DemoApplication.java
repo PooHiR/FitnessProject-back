c73abde591428981b5bf77c83c13954d4928b4bf
PooHiR <478071496@qq.com>
2024-06-11 20:03:35 +0800

    project init

---
+++ src/main/java/com/Ek0wraith/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.Ek0wraith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName: DemoApplication
+ * Package: com.Ek0wraith
+ * Description：TODO
+ *
+ * @Author Ek0wraith
+ * @Create 2024/6/11 19:52
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class,args);
+    }
+}
